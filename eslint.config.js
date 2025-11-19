8e2cabd32d36e3289a0dc93d58e16ec55cf953d5
Shubham Shinde <shubhamshinde281999@gmail.com>
2025-11-19 14:55:43 +0530

    added eslint

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+
+export default [
+  { ignores: ['dist', 'node_modules'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globalThis,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
+];
+
